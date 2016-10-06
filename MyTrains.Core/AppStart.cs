77801108096aed97128968c10efbecd882f79fc1
rohit.arun.pathak <rohit.arun.pathak@M5-DX-9TN8H92>
2016-10-06 18:20:31 +0530

    Add project files.

---
+++ MyTrains.Core/AppStart.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MyTrains.Core.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrains.Core
+{
+    public class AppStart : MvxNavigatingObject, IMvxAppStart
+    {
+        public async void Start(object hint = null)
+        {
+            throw new NotImplementedException();
+
+            var userService = Mvx.Resolve<IUserDataService>();
+            //await userService.
+        }
+    }
+}
